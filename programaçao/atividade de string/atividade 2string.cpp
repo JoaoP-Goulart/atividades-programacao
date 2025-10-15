58fb7a8843852c1558370209cca87e0c6e5e33fc
JoaoP-Goulart <jpgfaria.cruzeiro@gmail.com>
2025-10-15 16:00:47 -0300

    atualizaçao da atividade

--- programaçao/atividade de string/atividade 2string.cpp
+++ programaçao/atividade de string/atividade 2string.cpp
@@ -3,10 +3,37 @@
 using namespace std;
 
 int main() {
-	string palavra1;
-	cin >> palavra1;
+	string P;
+	cin >> P;
 	
-	cout << palavra1;
-   
+	int N;
+	cin >> N;
+	
+	string* lista = new string[N];
+	
+	for(int i = 0;i < N;i++){
+		cin >> lista[i];
+	}
+	
+	int cont = -1;
+
+	string palavra = " ";
+	
+	for(int i = 0;i < N; i++){
+		int rep = 0;
+		for(int j = 0;j < 5;j++){
+		if(lista[i][j] == P[j]){
+			rep++;
+		}
+		if(rep >= cont){
+			cont = rep;
+			palavra = lista[i];
+		}
+	}
+	}
+	cout << palavra;
+	
+	
+   delete[] lista;
     return 0;
 }
