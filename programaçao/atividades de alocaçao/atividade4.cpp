27c5471a0f6a20073e43810e1b81ad084d3be4ef
JoaoP-Goulart <jpgfaria.cruzeiro@gmail.com>
2025-10-23 17:34:17 -0300

    atividade 4 de alocaçao

---
+++ programaçao/atividades de alocaçao/atividade4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	
+	int* v = new int[N];
+	
+	for(int i = 0;i < N;i++){
+		cin >> v[i];
+	}
+	
+	int* vr = new int[N];
+	
+	int nl1, nl2;
+	cin >> nl1 >> nl2;
+	
+	for(int i = 0; i < N;i++){
+		if(v[i] == nl2){
+			
+	
+	
+	delete[] v;
+    return 0;
+}
