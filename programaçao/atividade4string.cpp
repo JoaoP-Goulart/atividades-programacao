38a053405d7c85c90e5b18b1b6f41f80f05577e8
JoaoP-Goulart <jpgfaria.cruzeiro@gmail.com>
2025-10-23 19:10:37 -0300

    a

--- programaçao/atividade4string.cpp
+++ programaçao/atividade4string.cpp
@@ -1,45 +1,38 @@
-
 #include <iostream>
- #include <cstring>
- 
+#include <string>
 using namespace std;
 
-int main (){
-	int N;
-	char letra;
-	
-	cin >> N;
-	
-	string* lista= new string[N];
+int main() {
+    int N;
+    cin >> N;
+    
+    string* lista = new string[N];
+    
+    for (int i = 0; i < N; i++) {
+        cin >> lista[i];
+    }
+    
+    char letra;
+    cin >> letra;
+    
+    int qntSemLetra = 0; 
+    
+    for (int i = 0; i < N; i++) {
+        bool temLetra = false; 
+        for (size_t j = 0; j < lista[i].size(); j++) {
+            if (lista[i][j] == letra) {
+                temLetra = true; 
+                break; 
+            }
+        }
+        
+        if (!temLetra) {
+            qntSemLetra++; 
+        }
+    }
+    
+    cout << qntSemLetra << endl;
 
-	for(int i=0 ; i < N; i++){
-		cin >> lista[i];
-	}
-	
-	cin >> letra;
-	int cont=0;
-	int m=0;
-
-	
-	for(int i=0 ; i < N; i++){
-		int TAM= lista[i].size();
-		for( int x=0 ; x < TAM; x++){
-			
-			if( lista[i][x] != letra){
-				cont++;
-			}
-			
-		}
-		if( cont== TAM){
-		m++;
-	}
-		cont=0;
-	}
-	
-	
-	cout << m;
-	
-
-	delete [] lista;
-	return 0;
+    delete[] lista;
+    return 0;
 }
