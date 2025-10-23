95b3eadde0da351239e0b126a91c5148643d38b0
JoaoP-Goulart <jpgfaria.cruzeiro@gmail.com>
2025-10-23 18:13:13 -0300

    atividade nova

--- programaçao/atividades de alocaçao/atividade4.cpp
+++ programaçao/atividades de alocaçao/atividade4.cpp
@@ -12,16 +12,30 @@ int main() {
 		cin >> v[i];
 	}
 	
-	int* vr = new int[N];
+	int n1, n2;
+	cin >> n1 >> n2;
 	
-	int nl1, nl2;
-	cin >> nl1 >> nl2;
+	int i1,i2;
 	
-	for(int i = 0; i < N;i++){
-		if(v[i] == nl2){
-			
+	for(int i = 0; i < N; i++){
+	if(v[i] == n1){
+		i1 = i;
+	}
+	if(v[i] == n2){
+		i2 = i;
+	}
+}
+int inicio = min(i1,i2)+1;
+int fim = max(i1,i2)-1;
+
+
 	
+	for(int i = fim; i >= inicio;i--){
+	cout << v[i] <<" ";
+}
+	cout << endl;
 	
 	delete[] v;
+
     return 0;
 }
