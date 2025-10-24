292a825a899d28b2be2d47f892fef9a8bd9a4cbd
JoaoP-Goulart <jpgfaria.cruzeiro@gmail.com>
2025-10-24 12:08:21 -0300

    atividade atualizada

--- programaçao/atividade de matriz/atividade14.cpp
+++ programaçao/atividade de matriz/atividade14.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-int BuscaBinaria(int v[],int inicio,int fim,int procurado){
-	
-		int meio = (inicio+fim)/2;
-		if(v[meio] == procurado){
-			return meio;
-		}
-		else if( v[meio] > procurado){
-			return BuscaBinaria(v,meio+1,fim,procurado);
-		}
-		else{
-			return BuscaBinaria(v,inicio,meio-1,procurado);
-		}
-	
-			return -1;
-		}
 
+int BuscaBinariaDecrescente(int v[], int inicio, int fim, int procurado) {
+    if (inicio > fim) {
+        return -1; 
+    }
+
+    int meio = (inicio + fim) / 2;
+
+    if (v[meio] == procurado) {
+        return meio;
+    } 
+    else if (v[meio] < procurado) {
+        
+        return BuscaBinariaDecrescente(v, inicio, meio - 1, procurado);
+    } 
+    else {
+        
+        return BuscaBinariaDecrescente(v, meio + 1, fim, procurado);
+    }
+}
 
 int main() {
-	
-	int N;
-	cin >> N;
-	
-	int* v = new int[N];
-	
-	for(int i = 0;i < N;i++){
-		cin >> v[i];
-	}
-	
-	int procurado;
-	cin >> procurado;
-	
-	cout << BuscaBinaria(v,0,N-1,procurado) << endl;
-	
-	
+    int N;
+    cin >> N;
+
+    int* v = new int[N];
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+
+    int procurado;
+    cin >> procurado;
+
+    cout << BuscaBinariaDecrescente(v, 0, N - 1, procurado) << endl;
+
     delete[] v;
     return 0;
 }
